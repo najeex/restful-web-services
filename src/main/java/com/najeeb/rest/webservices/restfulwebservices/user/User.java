955215c4bbc2f080cfab4762a32926bfe5dd111a
najeex <najeebagra@gmail.com>
2018-01-18 21:38:33 +0530

    adding cade for restful service

--- src/main/java/com/najeeb/rest/webservices/restfulwebservices/user/User.java
+++ src/main/java/com/najeeb/rest/webservices/restfulwebservices/user/User.java
@@ -1,14 +1,24 @@
 package com.najeeb.rest.webservices.restfulwebservices.user;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
 import java.util.Date;
 
+@ApiModel(description = "All details about the user.")
 public class User {
 
 
     private Integer id;
 
+    @Size(min=2, message = "Name Should have atleast 2 chaaracter")
+    @ApiModelProperty(notes = "Name Should have atleast 2 chaaracter")
     private String name;
 
+    @Past
+    @ApiModelProperty(notes="Birth date should be in the past ")
     private Date birthday;
 
     protected User(){
