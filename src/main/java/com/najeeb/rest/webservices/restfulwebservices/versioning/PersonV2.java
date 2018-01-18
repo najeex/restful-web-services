12cd6022b5f7b84638ed9d9431b1b984afb50b4e
najeex <najeebagra@gmail.com>
2018-01-19 02:34:35 +0530

    final commit...!!! course was awesome

---
+++ src/main/java/com/najeeb/rest/webservices/restfulwebservices/versioning/PersonV2.java
@@ -0,0 +1,19 @@
+package com.najeeb.rest.webservices.restfulwebservices.versioning;
+
+public class PersonV2 {
+
+    private Name name;
+
+    public PersonV2(Name name) {
+        this.name = name;
+    }
+
+    public Name getName() {
+        return name;
+    }
+
+    public void setName(Name name) {
+        this.name = name;
+    }
+}
+
